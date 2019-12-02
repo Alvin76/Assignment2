46090427b116b47c5473afc7201ce4119ccb11a7
Adam Davies <45388618+Alvin76@users.noreply.github.com>
2019-12-02 13:28:41 -0500

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -134,9 +134,9 @@ function page_title()
 function page_content()
 {
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-    $path = getcwd() . '/' . config('content_path') . '/' . $page . '.phtml';
+    $path = getcwd() . '/' . config('Content_path') . '/' . $page . '.phtml';
     if (! file_exists($path)) {
-        $path = getcwd() . '/' . config('content_path') . '/404.phtml';
+        $path = getcwd() . '/' . config('Content_path') . '/404.phtml';
     }
     echo file_get_contents($path);
 }
